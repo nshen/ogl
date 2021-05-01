b2bc5500221b204b06fdac27975e8aa6f4dfe5e9
nshen <nshen121@gmail.com>
2021-05-01 18:55:28 +0800

    update Color

--- src/math/Color.ts
+++ src/math/Color.ts
@@ -1,5 +1,3 @@
-import * as ColorFunc from './functions/ColorFunc';
-
 // Color stored as an array of RGB decimal values (between 0 > 1)
 // Constructor and set method accept following formats:
 // new Color() - Empty (defaults to black)
@@ -9,19 +7,61 @@ import * as ColorFunc from './functions/ColorFunc';
 // new Color('#ccc') - Short-hand Hex string
 // new Color(0x4f27e8) - Number
 // new Color('red') - Color name string (short list in ColorFunc.js)
+const NAMES = {
+    black: '#000000',
+    white: '#ffffff',
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+    fuchsia: '#ff00ff',
+    cyan: '#00ffff',
+    yellow: '#ffff00',
+    orange: '#ff8000',
+};
 
+function hexToRGB(hex) {
+    if (hex.length === 4) hex = hex[0] + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
+    const rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if (!rgb) console.warn(`Unable to convert hex string ${hex} to rgb values`);
+    return [parseInt(rgb[1], 16) / 255, parseInt(rgb[2], 16) / 255, parseInt(rgb[3], 16) / 255];
+}
+
+function numberToRGB(num) {
+    num = parseInt(num);
+    return [((num >> 16) & 255) / 255, ((num >> 8) & 255) / 255, (num & 255) / 255];
+}
+
+function parseColor(color: any = null) {
+    // Empty
+    if (!color) return [0, 0, 0];
+
+    // Decimal
+    if (arguments.length === 3) return arguments;
+
+    // Number
+    if (!isNaN(color)) return numberToRGB(color);
+
+    // Hex
+    if (color[0] === '#') return hexToRGB(color);
+
+    // Names
+    if (NAMES[color.toLowerCase()]) return hexToRGB(NAMES[color.toLowerCase()]);
+
+    console.warn('Color format not recognised');
+    return [0, 0, 0];
+}
 export class Color extends Array<number> {
     constructor();
     constructor(color: [number, number, number]);
     constructor(color: number, g: number, b: number);
     constructor(color: string);
-    constructor(color: ColorFunc.ColorNames);
+    constructor(color: keyof typeof NAMES);
     constructor(color: number);
     constructor(color: any = null) {
         if (Array.isArray(color)) {
             super(...color);
         } else {
-            super(...ColorFunc.parseColor(...arguments));
+            super(...parseColor(...arguments));
         }
     }
 
@@ -51,7 +91,7 @@ export class Color extends Array<number> {
 
     set(color) {
         if (Array.isArray(color)) return this.copy(color);
-        return this.copy(ColorFunc.parseColor(...arguments));
+        return this.copy(parseColor(...arguments));
     }
 
     copy(v) {
